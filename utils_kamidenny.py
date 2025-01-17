98e4630b66849960d03061bf91297718fcf4713d
Kami Nicole Denny <120343004+dennykami1@users.noreply.github.com>
2025-01-17 14:39:25 -0600

    Update utils_kamidenny.py
    Finalized

--- utils_kamidenny.py
+++ utils_kamidenny.py
@@ -3,9 +3,10 @@ Module: utils_kamidenny
 
 Purpose: Reusable Module for My Analytics Projects
 
-Description: This module provides a byline for my analytics projects. 
-When we work hard to write useful code, we want it to be reusable.
-A good byline could be used in every Python analytics project we do.
+Description: This module provides a great example of how to create a
+byline for my analytics projects. When we work hard to write useful code,
+we want it to be reusable. This module displays how statistics, variables,
+and F-Strings can be incorporated into our code.
 
 Author: Kami Denny
 
