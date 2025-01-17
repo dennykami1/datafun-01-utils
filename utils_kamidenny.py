8ee3b22468cf89dfcd7ac3e7b813ff72f77fe1e6
Kami Nicole Denny <120343004+dennykami1@users.noreply.github.com>
2025-01-17 14:33:06 -0600

    Update utils_kamidenny.py
    Update stats

--- utils_kamidenny.py
+++ utils_kamidenny.py
@@ -41,7 +41,8 @@ skills_offered: list = ["Data Analysis", "Machine Learning", "Business Intellige
 benefits_offered: list = ["Unlimited PTO", "Health Insurance", "Life Insurance", "Remote Work Options"]
 
 # declare a list of numbers so we can illustrate statistics skills
-employee_satisfaction_scores: list = [4.8, 4.6, 4.9, 5.0, 4.7]
+employee_satisfaction_scores: list = [4.3, 4.6, 4.9, 4.5, 4.2, 4.0]
+years_until_promotion: list = [1.8, 2.4, 1.1, 1.3, 2.0, 1.6]
 
 # Calculate basic statistics using built-in Python functions and the statistics module
 min_score: float = min(employee_satisfaction_scores)  
@@ -49,21 +50,31 @@ max_score: float = max(employee_satisfaction_scores)
 mean_score: float = statistics.mean(employee_satisfaction_scores)  
 stdev_score: float = statistics.stdev(employee_satisfaction_scores)
 
+min_years: float = min(years_until_promotion)  
+max_years: float = max(years_until_promotion)  
+mean_years: float = statistics.mean(years_until_promotion)  
+stdev_years: float = statistics.stdev(years_until_promotion)
+
 # Use a Python formatted string (f-string) to show information
 byline: str = f"""
 ---------------------------------------------------------
 Stellar Analytics: Delivering Professional Insights
 ---------------------------------------------------------
-Skills Offered:               {skills_offered}
-Years in Operation:           {years_in_operation}
-Benefits Offered:             {benefits_offered}
-Offers 401k Matching:         {has_401k_matching}
-Number of Paid Holidays:      {count_of_holidays}
-Employee Satisfaction Scores: {employee_satisfaction_scores}
-Minimum Satisfaction Score:   {min_score}
-Maximum Satisfaction Score:   {max_score}
-Mean Satisfaction Score:      {mean_score:.2f}
-Standard Deviation of Satisfaction Scores: {stdev_score:.2f}
+Skills Offered:              {skills_offered}
+Years in Operation:          {years_in_operation}
+Benefits Offered:            {benefits_offered}
+Offers 401k Matching:        {has_401k_matching}
+Number of Paid Holidays:     {count_of_holidays}
+Employee Satisfaction Scores:  {employee_satisfaction_scores}
+Minimum Satisfaction Score:    {min_score}
+Maximum Satisfaction Score:    {max_score}
+Mean Satisfaction Score:       {mean_score:.2f}
+Standard Deviation of Satisfaction Scores:  {stdev_score:.2f}
+Years Until Employees Achieve Promotion:    {years_until_promotion}
+Minimum Years until Promotion:              {min_years}
+Maximum Years until Promotion:              {max_years}
+Mean Years until Promotion:                 {mean_years:.2f}
+Standard Deviation Years until Promotion:   {stdev_years:.2f}
 """
 
 #####################################
